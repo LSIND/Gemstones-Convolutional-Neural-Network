fe383e41c46baad69713ebfe79dff814da10b3cd
Daria Chemkaeva <l_s_ind@hotmail.com>
2022-04-07 17:21:59 +0300

    Fix_RenameFiles

--- 1_Fetch_data/2_Rename_Files.py
+++ 1_Fetch_data/2_Rename_Files.py
@@ -7,30 +7,42 @@ import sys
 def main():
     userpath = sys.argv[1]
     try:
-        with os.scandir(userpath) as rootfolder:
-            for entry in rootfolder:
-                if(entry.is_dir()):
-                    f_path= os.path.join(userpath, entry.name)
-                    name = entry.name.lower()
-                    list_files = os.listdir(f_path)
-                    print(f_path)
+        if(os.path.isdir(userpath)):        
+            with os.scandir(userpath) as rootfolder:
+                for entry in rootfolder:
+                    if(entry.is_dir()):
+                        f_path = os.path.join(userpath, entry.name)                        
+                        name = entry.name.lower()
+                        listfiles = os.listdir(f_path)
+                        #print(listfiles)
+                        
+                        j = 0
+                        dst_filenames = []
+                        if (len(listfiles) > 0):
+                            print(f_path, ':', len(listfiles))
+                            for filename in listfiles:
+                                file, file_ext = os.path.splitext(filename)
+                                dst_filename = f'{name}_{j}{file_ext.lower()}'
+                                dst_filenames.append(dst_filename)
+                                j+=1
+                            
+                            #пересечение множеств
+                            inter = set(dst_filenames)& set(listfiles)
+                            print(inter)
+                            
+                            dst_filenamesF = [el for el in dst_filenames if el not in inter]
+                            listfilesF = [el for el in listfiles if el not in inter]
+                            
+                            finalNames = list(zip(listfilesF, dst_filenamesF))
+                            print('To rename:', len(finalNames))
+                            
+                            for filename in finalNames:                                
+                                #print(os.path.join(f_path, filename[0]))
+                                #print(os.path.join(f_path, filename[1]))
+                                os.rename(os.path.join(f_path, filename[0]), os.path.join(f_path, filename[1]))
 
-                    j = 0
-                    for filename in list_files: 
-                        file, file_extension = os.path.splitext(filename)
-                        dst = "{}_{}{}".format(name, j, file_extension.lower())
-                                         
-                        while dst in list_files:
-                            list_files.remove(dst)
-                            print('such file already exists: ', dst)
-                            j+=1
-                            dst = "{}_{}{}".format(name, j, file_extension.lower())
-                                    
-                        else:
-                            if len(list_files)!=0:
-                                print(filename, '--->\t', dst)
-                                os.rename(os.path.join(f_path,filename), os.path.join(f_path,dst)) 
-                            j+=1
+        else:
+            print(f'error: {userpath} incorrect')
 
     except Exception as e:
         print(str(e))
